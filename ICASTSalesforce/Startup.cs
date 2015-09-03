11296c5c69e6500ba1be3859264fb2e7864412e1
Patrick Yee <g0th1cazn@gmail.com>
2015-09-03 14:14:07 -0600

    Creating the ASP.NET Project
    A ICAST website using the Salesforce WSDL.  Just creating the project

---
+++ ICASTSalesforce/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ICASTSalesforce.Startup))]
+namespace ICASTSalesforce
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
